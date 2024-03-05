6bf1b157120f8fceb2a2a2068b7b38cca7e6e870
gabri <gabri@172.20.0.98>
2024-03-05 13:34:22 +0100

    Aufgabe 2, noch nicht ganz fertig

---
+++ src/arrayList/VerlusteManipulieren.java
@@ -0,0 +1,22 @@
+package arrayList;
+
+import java.util.List;
+
+public class VerlusteManipulieren {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public void trimNonGrowingNumbers(List<Double>numbers) {
+		
+		for(int i = 0; i < numbers.size(); i++) {
+			if(!(numbers.get(i) == i+1)) {
+				// LÖSCHEN
+			}
+		}
+		
+	}
+
+}
