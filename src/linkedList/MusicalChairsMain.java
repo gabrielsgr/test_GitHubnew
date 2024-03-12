73edbcfa58e270573745d1301e843efdcce34b44
gabri <gabri@172.20.0.98>
2024-03-12 12:57:37 +0100

    LinkedList finish

---
+++ src/linkedList/MusicalChairsMain.java
@@ -0,0 +1,15 @@
+package linkedList;
+
+public class MusicalChairsMain {
+
+	public static void main(String[] args) {
+		
+		MusicalChairs mc1 = new MusicalChairs("Leo", "Max", "Paul", "Anna");
+		MusicalChairs mc2 = new MusicalChairs("Madeleine", "Benedikt");
+		
+		System.out.println(mc1);
+		System.out.println(mc2);
+	}
+
+
+}
